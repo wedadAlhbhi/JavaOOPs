ea394093ea3ad1b5c65043c9e6a4b1753956ae5d
Wedad <wedad.alhabshi@ese.gov.ae>
2022-06-30 14:18:32 +0400

    fun01

---
+++ JavaBasePhase1/src/fun/HiWorld.java
@@ -0,0 +1,10 @@
+package fun;
+
+public class HiWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ JavaBasePhase1/src/fun/test1.java
@@ -0,0 +1,11 @@
+package fun;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Wedad, I am Java Developer");
+
+	}
+
+}
